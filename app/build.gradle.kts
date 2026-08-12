2ef5b9715e6f40716446d1799db699efd88f26e2
SparrashSharrma <sparsh5sharma7@gmail.com>
2026-08-12 23:53:50 +0530

    Bump version code and name to 5 and 1.3.6

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "com.rentmanager.app"
         minSdk = 24
         targetSdk = 35
-        versionCode = 4
-        versionName = "1.3.3.3"
+        versionCode = 5
+        versionName = "1.3.6"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
